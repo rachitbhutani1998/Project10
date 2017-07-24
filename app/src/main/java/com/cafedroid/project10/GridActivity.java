3024baf78726c599c2aee05e6fabd037eb111771
rachit <rachitbhutani1998@gmail.com>
2017-07-24 13:25:41 +0530

    added all projects to the grid

--- app/src/main/java/com/cafedroid/project10/GridActivity.java
+++ app/src/main/java/com/cafedroid/project10/GridActivity.java
@@ -21,10 +21,15 @@ public class GridActivity extends AppCompatActivity {
         GridView grid=(GridView) findViewById(R.id.grid);
         ArrayList<Projects> projectList= new ArrayList<>();
         projectList.add(new Projects("Single Screen",R.drawable.ic_smartphone_white_24dp,R.color.blue));
-        projectList.add(new Projects("Music App",R.drawable.ic_music_note_white_24dp,R.color.colorAccent));
-        projectList.add(new Projects("Inventory",R.drawable.ic_build_white_24dp,R.color.inventory));
+        projectList.add(new Projects("Court Counter",R.drawable.ic_plus_one_white_24dp,R.color.football));
         projectList.add(new Projects("Quiz App",R.drawable.ic_help_white_24dp,R.color.quiz));
-        projectList.add(new Projects("Court Counter",R.drawable.ic_build_white_24dp,R.color.football));
+        projectList.add(new Projects("Music App",R.drawable.ic_music_note_white_24dp,R.color.colorAccent));
+        projectList.add(new Projects("Report Card",R.drawable.ic_content_paste_white_24dp,R.color.report));
+        projectList.add(new Projects("Tour Guide",R.drawable.ic_directions_white_24dp,R.color.tour));
+        projectList.add(new Projects("Book Listing",R.drawable.ic_book_white_24dp,R.color.book));
+        projectList.add(new Projects("News App",R.drawable.ic_featured_play_list_white_24dp,R.color.news));
+        projectList.add(new Projects("Habit Tracker",R.drawable.ic_videogame_asset_white_24dp,R.color.habbit));
+        projectList.add(new Projects("Inventory",R.drawable.ic_build_white_24dp,R.color.inventory));
         mAdapter=new GridAdapter(getApplicationContext(),projectList);
         grid.setAdapter(mAdapter);
         grid.setColumnWidth(width/2);
